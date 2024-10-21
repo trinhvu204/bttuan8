7552f0acfb6b504abe9bb446e411051f44bbf0ec
trinhvu204 <133593232+trinhvu204@users.noreply.github.com>
2024-10-21 11:29:21 +0700

    Add files via upload

---
+++ TUAN8/BTTuan8/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace BTTuan8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvStudent = new System.Windows.Forms.DataGridView();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.cmbNganh = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.schoolDBDataSet = new BTTuan8.SchoolDBDataSet();
+            this.studentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.studentsTableAdapter = new BTTuan8.SchoolDBDataSetTableAdapters.StudentsTableAdapter();
+            this.studentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fullNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.majorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnlui = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.schoolDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvStudent
+            // 
+            this.dgvStudent.AutoGenerateColumns = false;
+            this.dgvStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.studentIDDataGridViewTextBoxColumn,
+            this.fullNameDataGridViewTextBoxColumn,
+            this.ageDataGridViewTextBoxColumn,
+            this.majorDataGridViewTextBoxColumn});
+            this.dgvStudent.DataSource = this.studentsBindingSource;
+            this.dgvStudent.Location = new System.Drawing.Point(385, 97);
+            this.dgvStudent.Name = "dgvStudent";
+            this.dgvStudent.RowHeadersWidth = 51;
+            this.dgvStudent.RowTemplate.Height = 24;
+            this.dgvStudent.Size = new System.Drawing.Size(600, 278);
+            this.dgvStudent.TabIndex = 0;
+            this.dgvStudent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(119, 46);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(179, 22);
+            this.txtFullName.TabIndex = 1;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(119, 114);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(179, 22);
+            this.txtAge.TabIndex = 1;
+            // 
+            // cmbNganh
+            // 
+            this.cmbNganh.FormattingEnabled = true;
+            this.cmbNganh.Items.AddRange(new object[] {
+            "Công Nghệ Thong Tin",
+            "Maketting"});
+            this.cmbNganh.Location = new System.Drawing.Point(119, 180);
+            this.cmbNganh.Name = "cmbNganh";
+            this.cmbNganh.Size = new System.Drawing.Size(179, 24);
+            this.cmbNganh.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnSua);
+            this.groupBox1.Controls.Add(this.btnXoa);
+            this.groupBox1.Controls.Add(this.btnThem);
+            this.groupBox1.Controls.Add(this.txtFullName);
+            this.groupBox1.Controls.Add(this.cmbNganh);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Location = new System.Drawing.Point(25, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 314);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thong tin sinh vien";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(22, 246);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(214, 246);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(119, 246);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 3;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FullName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nganh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Age";
+            // 
+            // schoolDBDataSet
+            // 
+            this.schoolDBDataSet.DataSetName = "SchoolDBDataSet";
+            this.schoolDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentsBindingSource
+            // 
+            this.studentsBindingSource.DataMember = "Students";
+            this.studentsBindingSource.DataSource = this.schoolDBDataSet;
+            // 
+            // studentsTableAdapter
+            // 
+            this.studentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // studentIDDataGridViewTextBoxColumn
+            // 
+            this.studentIDDataGridViewTextBoxColumn.DataPropertyName = "StudentID";
+            this.studentIDDataGridViewTextBoxColumn.HeaderText = "StudentID";
+            this.studentIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.studentIDDataGridViewTextBoxColumn.Name = "studentIDDataGridViewTextBoxColumn";
+            this.studentIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.studentIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // fullNameDataGridViewTextBoxColumn
+            // 
+            this.fullNameDataGridViewTextBoxColumn.DataPropertyName = "FullName";
+            this.fullNameDataGridViewTextBoxColumn.HeaderText = "FullName";
+            this.fullNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.fullNameDataGridViewTextBoxColumn.Name = "fullNameDataGridViewTextBoxColumn";
+            this.fullNameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // ageDataGridViewTextBoxColumn
+            // 
+            this.ageDataGridViewTextBoxColumn.DataPropertyName = "age";
+            this.ageDataGridViewTextBoxColumn.HeaderText = "age";
+            this.ageDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            this.ageDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // majorDataGridViewTextBoxColumn
+            // 
+            this.majorDataGridViewTextBoxColumn.DataPropertyName = "Major";
+            this.majorDataGridViewTextBoxColumn.HeaderText = "Major";
+            this.majorDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.majorDataGridViewTextBoxColumn.Name = "majorDataGridViewTextBoxColumn";
+            this.majorDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(704, 402);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnlui
+            // 
+            this.btnlui.Location = new System.Drawing.Point(812, 402);
+            this.btnlui.Name = "btnlui";
+            this.btnlui.Size = new System.Drawing.Size(75, 23);
+            this.btnlui.TabIndex = 4;
+            this.btnlui.Text = "Lui";
+            this.btnlui.UseVisualStyleBackColor = true;
+            this.btnlui.Click += new System.EventHandler(this.btnlui_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 450);
+            this.Controls.Add(this.btnlui);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvStudent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.schoolDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvStudent;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ComboBox cmbNganh;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private SchoolDBDataSet schoolDBDataSet;
+        private System.Windows.Forms.BindingSource studentsBindingSource;
+        private SchoolDBDataSetTableAdapters.StudentsTableAdapter studentsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fullNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn majorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnlui;
+    }
+}
+
---
+++ TUAN8/BTTuan8/Form1.cs
@@ -0,0 +1,108 @@
+﻿using BTTuan8.SchoolDBDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTTuan8
+{
+    public partial class Form1 : Form
+    {
+        public SchoolDBDataSet context =  new SchoolDBDataSet();
+        private BindingSource bindingSource1 = new BindingSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'schoolDBDataSet.Students' table. You can move, or remove it, as needed.
+            this.studentsTableAdapter.Fill(this.schoolDBDataSet.Students);
+           bindingSource1.DataSource = schoolDBDataSet.Students;
+            dgvStudent.DataSource = bindingSource1;
+            txtFullName.DataBindings.Add("Text", bindingSource1, "FullName");
+            txtAge.DataBindings.Add("Text", bindingSource1, "Age");
+            cmbNganh.DataSource = new BindingSource(GetNganh(), null);
+            cmbNganh.DisplayMember = "Value"; // Tên hiển thị trong ComboBox
+            cmbNganh.ValueMember = "Key"; // Giá trị thực sự sẽ được lưu vào cơ sở dữ liệu
+            cmbNganh.DataBindings.Add("SelectedValue", bindingSource1, "Major"); // Liên kết với trường Major
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            DataRow newStudent = schoolDBDataSet.Students.NewRow();
+            newStudent["FullName"] = txtFullName.Text;
+            newStudent["Age"] = int.Parse(txtAge.Text);
+            newStudent["Major"] = cmbNganh.SelectedValue.ToString();
+            
+            schoolDBDataSet.Students.Rows.Add(newStudent);
+            studentsTableAdapter.Update(schoolDBDataSet.Students);
+            bindingSource1.ResetBindings(false);
+
+
+        }
+        private Dictionary<string, string> GetNganh()
+        {
+            return new Dictionary<string, string>
+            {
+                { "CNTT", "Công nghệ Thông tin" },
+                { "CK", "Cơ khí" },
+                { "MK", "Marketing" }
+            };
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (bindingSource1.Current != null)
+            {
+                DataRowView currentRow = (DataRowView)bindingSource1.Current;
+                currentRow["FullName"] = txtFullName.Text;
+                currentRow["Age"] = int.Parse(txtAge.Text);
+                currentRow["Major"] = cmbNganh.SelectedValue; 
+
+                
+                studentsTableAdapter.Update(schoolDBDataSet.Students);
+                bindingSource1.ResetBindings(false);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (bindingSource1.Current != null)
+            {
+                DataRowView currentRow = (DataRowView)bindingSource1.Current;
+                currentRow.Delete();
+
+                
+                studentsTableAdapter.Update(schoolDBDataSet.Students);
+
+              
+                bindingSource1.ResetBindings(false);
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MoveNext();
+        }
+
+        private void btnlui_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MovePrevious();
+        }
+    }
+}
